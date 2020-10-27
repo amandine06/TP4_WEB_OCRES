174cf87f8cdbda729fb8c0cc53d1bf774678cdae
amandine06 <63580735+amandine06@users.noreply.github.com>
2020-10-27 08:39:46 +0100

    ajout code

--- fb-ultra-high/src/App.js
+++ fb-ultra-high/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -20,6 +20,42 @@ function App() {
       </header>
     </div>
   );
-}
 
 export default App;
+*/
+
+import React, { Component } from 'react';
+
+class Profil extends React.Component {
+  render() {
+    return (
+      <div className="profil">
+        <h1>PHOTO</h1> 
+        <h6>Prénom : </h6>
+        <h6>Nom : </h6>
+        <h6>Date de naissance : </h6>
+        <button>Change style</button>
+      </div>
+    )
+  }
+}
+
+class Interface extends React.Component {
+  renderProfil(i) {
+    return <Profil />
+  }
+
+  render() {
+    return (
+      <div className="interface">
+        <h3>Mon navigateur</h3>
+        <button>Jeanne</button>
+        <button>Martine</button>
+        <button>Claude</button>
+      </div>
+    )
+  }
+
+}
+
+export default Interface;
--- fb-ultra-high/src/index.js
+++ fb-ultra-high/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+import { StrictMode } from 'react';
+import Interface from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Interface />
   </React.StrictMode>,
   document.getElementById('root')
 );
